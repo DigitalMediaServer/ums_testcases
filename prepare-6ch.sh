2b43b25263c061219da5232e3b0a7065571a903a
Stasinos Konstantopoulos <stasinos@konstant.gr>
2018-12-31 18:36:52 +0200

    Also made 6-channel test files

--- prepare-6ch.sh
+++ prepare-6ch.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Upmix to create the 6-channel content
+
 VINPUT="-i orig/pexels-video-857601.mkv"
 AINPUT="-ss 0 -t 15.42 -i orig/freesound-365215.wav"
 
@@ -19,7 +21,6 @@ acodecs["aacHE"]="-c:a libfdk_aac -profile:a aac_he"
 acodecs["mp3"]="-c:a libmp3lame -b:a 256k"
 acodecs["ac3"]="-c:a ac3        -b:a 256k -ar 48000"
 acodecs["mp2"]="-c:a mp2        -b:a 320k"
-acodecs["opus"]="-c:a libopus -b:a 128k"
 acodecs["vorbis"]="-c:a libvorbis -aq 4"
 acodecs["flac"]="-c:a flac"
 
@@ -27,30 +28,26 @@ declare -A vcontainers
 declare -A acontainers
 
 vcontainers["mkv"]="h264 h265 xvid"
-acontainers["mkv"]="aacLC aacHE ac3 mp3 mp2 opus vorbis flac"
+acontainers["mkv"]="aacLC aacHE ac3 opus vorbis flac"
 
 vcontainers["avi"]="xvid"
-acontainers["avi"]="mp3 ac3 mp2"
+acontainers["avi"]="ac3"
 
 vcontainers["mp4"]="h264 xvid"
-acontainers["mp4"]="ac3 mp3 aacLC aacHE mp2"
+acontainers["mp4"]="ac3 aacLC aacHE"
 
 for fmt in ${!vcontainers[@]}; do
     for vcod in ${vcontainers[${fmt}]}; do
 	for acod in ${acontainers[${fmt}]}; do
-	    ffmpeg -n $VINPUT $AINPUT ${vcodecs[${vcod}]} $VSCALE ${acodecs[${acod}]} $vcod-$acod-2ch.$fmt
+	    echo ffmpeg -n $VINPUT $AINPUT -filter_complex "[1:a]pan=5.1(side)|FL=FL|FR=FR|LFE<FL+FR|SL=FL|SR=FR[a]" -map 0:v -map -1:a -map "[a]" ${vcodecs[${vcod}]} $VSCALE ${acodecs[${acod}]} $vcod-$acod-6ch.$fmt
 	done
     done
 done
 
 exit
 
-samples:
+TODO:
 
-ffmpeg -i orig/pexels-video-857601.mkv -i orig/freesound-365215.wav  -ss 0 -t 15.42 -c:v libx264 -profile:v main -vf scale=640:-1 -c:a libfdk_aac  -b:a 256k  h264-aac-2ch.mkv
+MKV-{H264,H265,XviD}-DTS-6ch.mkv
 
-ffmpeg -i orig/pexels-video-857601.mkv -i orig/freesound-365215.wav  -ss 0 -t 15.42 -c:v libxvid -vf scale=640:-1 -c:a libmp3lame -b:a 256k xvid-mp3-2ch.avi
-
-ffmpeg -i orig/pexels-video-857601.mkv  -ss 0 -t 15.42 -i orig/freesound-365215.wav -c:v libxvid -vf scale=640:-1 -c:a ac3 -b:a 256k -ar 48000 xvid-ac3-2ch.avi
-
-
+ffmpeg -n -i orig/pexels-video-857601.mkv -ss 0 -t 15.42 -i orig/freesound-365215.wav -filter_complex [1:a]pan=5.1(side)|FL=FL|FR=FR|LFE<FL+FR|SL=FL|SR=FR[a] -map 0:v -map -1:a -map [a] -vf scale=640:-1 -c:a libopus -b:a 128k xvid-opus-6ch.mkv
